4b7bc6d99b5ec822ba1a043169b093a244bbf8ba
Chirag <chiragsimkhada@gmail.com>
2021-12-01 12:37:43 +0545

    Added proper documentation

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 function getOutputValue() {
-	return document.getElementById("output-value").innerText;
+	return document.getElementById("output-value").innerText; // Return value from display-box
 }
 
-function output(number) {
-	if (number == "") {
+function output(number) { //function to display output in formatted manner
+	if (number == "") { //Handling the base case: if box is empty, display number as it is
 		document.getElementById("output-value").innerText = number;
-	} else {
+	} else { //Else, call getFormattedNumberValue function and format the entered number
 		document.getElementById("output-value").innerText = getFormattedNumberValue(number);
 	}
 }
 
-function getHistoryValue() {
-	return document.getElementById("history-value").innerText;
+function getHistoryValue() { //Function to retrive previous Output
+	return document.getElementById("history-value").innerText; //Get value from if "history-value"
 }
 
-function history(num) {
-	document.getElementById("history-value").innerText = num;
+function history(num) { // Function to set history value
+	document.getElementById("history-value").innerText = num; //Set history value to num
 }
 
-function getFormattedNumberValue(number) {
-	if (number == "-") {
+function getFormattedNumberValue(number) { //Function to format numbers
+	if (number == "-") { // Handling base case, i.e. if num == "-", do nothing
 		return "";
 	}
-	let num = Number(number);
-	let output = num.toLocaleString("en");
-	return output;
+	let num = Number(number); //COnverting the input to number type
+	let output = num.toLocaleString("en"); // Changing number to locale string of english
+	return output; //Returns the iutput
 }
 
 function getReverseNumber(number) {
-	return Number(number.replace(/,/g, ''));
+	return Number(number.replace(/,/g, '')); // Function to reverse the given number
 }
 
-let output = document.getElementsByClassName("operator");
+let output = document.getElementsByClassName("operator"); // Get value stored in "Operator" class
 
 for (let i = 0; i < output.length; i++) {
-	output[i].addEventListener('click', function () {
-		if (this.id == "clear") {
-			History("");
-			Output("");
-		} else if (this.id == "backspace") {
-			let output = getReverseNumber(getOutputValue()).toString();
+	output[i].addEventListener('click', function () { //Add click event listener to the members of "operator" class
+		if (this.id == "clear") { // Defining task of clear function
+			History(""); //Clears history
+			Output(""); //Clears output
+		} else if (this.id == "backspace") { //Defining work of "backspace" key
+			let output = getReverseNumber(getOutputValue()).toString(); //reverse current output
 			if (output) {
-				output = output.substr(0, output.length - 1);
-				Output(output);
+				output = output.substr(0, output.length - 1); //Removes lastmost character from reversed string i.e first number from number in output display
+				Output(output); //Sends number to output
 			}
 		} else {
-			let result = getOutputValue();
-			let past = getHistoryValue();
-			if (result == "" && past != "") {
-				if (isNaN(past[past.length - 1])) {
-					past = past.substr(0, past.length - 1);
+			let result = getOutputValue(); //Stores Output value
+			let past = getHistoryValue(); //Stores history value
+			if (result == "" && past != "") { //Handling base case: if current result is empty/null and past is not
+				if (isNaN(past[past.length - 1])) { //if past-1 was null/empty
+					past = past.substr(0, past.length - 1); //set past to following
 				}
 			}
-			if (result != "" || past != "") {
+			if (result != "" || past != "") { //Either case for above condition
 				result = result == "" ? result : getReverseNumber(result);
 				past = past + result;
-				if (this.id == "=") {
-					let result = eval(past);
-					Output(result);
-					History("");
+				if (this.id == "=") { //If current element is "="
+					let result = eval(past); //Evaluate string on screen
+					Output(result); //Display the result as output
+					History(""); //Set history to ""
 				} else {
-					past = past + this.id;
-					History(past);
-					Output("");
+					past = past + this.id; //Add current id to past
+					History(past); //set past to history
+					Output(""); // set output yo null
 				}
 			}
 		}
